99118879d55beacd2f9c0b0f8fa1cca000d7826c
Minh-Nghia Nguyen <elvin.ss127@gmail.com>
2017-01-07 11:51:08 +0000

    Add tile position calculation

--- TBD/Assets/Script/GameMechanic/GameManager.cs
+++ TBD/Assets/Script/GameMechanic/GameManager.cs
@@ -1,25 +1,28 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameManager : MonoBehaviour {
-    GameObject nhat;
-    GameObject nghia ;
-    private float timeRemaining = 4;
-    // Use this for initialization
-    void Start () {
-         nhat = GameObject.Find("LinhCuaNhat");
-         nghia = GameObject.Find("LinhCuaNghia");
- 
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-        timeRemaining -= Time.deltaTime;
-        float seconds = Mathf.Floor(timeRemaining % 60);
-
-          if(timeRemaining<2)
-         nhat.transform.position = Vector3.MoveTowards(nhat.transform.position, nghia.transform.position, 100000 * Time.deltaTime);
-    }
-   
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class GameManager : MonoBehaviour {
+//    GameObject nhat;
+//	GameObject nghia;
+//	GameObject ground;
+//    private float timeRemaining = 4;
+//    // Use this for initialization
+//    void Start () {
+//         nhat = GameObject.Find("LinhCuaNhat");
+//		print (nhat);
+//         nghia = GameObject.Find("LinhCuaNghia"); 
+//
+//    }
+//
+//    // Update is called once per frame
+//    void Update () {
+//        timeRemaining -= Time.deltaTime;
+//        float seconds = Mathf.Floor(timeRemaining % 60);
+//
+//		if (timeRemaining < 2) {
+//			nhat.transform.position = Vector3.MoveTowards (nhat.transform.position, nghia.transform.position, 10 * Time.deltaTime);
+//			nghia.transform.position = Vector3.MoveTowards (nghia.transform.position, nhat.transform.position, 10 * Time.deltaTime);
+//		}
+//    }
+//   
+//}
